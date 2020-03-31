492580b8db77eecf8b693c451f2fef3af87d6dae
Aline Cavalcante <Aline-Cavalcante@live.nmit.ac.nz>
2020-03-31 18:42:47 +1300

    Admin Mod, User and Home edits. Router creation

---
+++ src/router.js
@@ -0,0 +1,50 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Login from './views/Login.vue';
+import Register from './views/Register.vue';
+
+Vue.use(Router);
+
+export const router = new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path:'/home',
+            component: Home
+        },
+        {
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/register',
+            component: Register
+        },
+        {
+            path: '/profile',
+            name: 'profile',
+            component: ()=> import('./views/Profile.vue')
+        },
+        {
+            path: '/admin',
+            name: 'admin',
+            component: () => import('./views/BoardAdmin.vue')
+        },
+        {
+            path: '/mod',
+            name: 'moderator',
+            component: () => import('./views/BoardModerator.vue')
+        },
+        {
+            path: '/user',
+            name: 'user',
+            component: () => import('./views/BoardUser.vue')
+        }     
+    ]
+});
